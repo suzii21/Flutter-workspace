f85b92fb3556a0241093cdbbe3f34d36ab64f8f6
suzii21 <136336201+suzii21@users.noreply.github.com>
2023-11-21 18:01:47 +0530

    Add files via upload

---
+++ ASSIGNMENT/MODULE-2/QUE-51.dart
@@ -0,0 +1,16 @@
+//Create a program using List
+
+void main()
+{
+  var num_list = [0,1,2,3];
+  print("original list : $num_list");
+  num_list.add(4);
+
+  print("add  element : $num_list");
+
+  num_list.remove(0);
+
+  print("remove element : $num_list");
+
+
+}
